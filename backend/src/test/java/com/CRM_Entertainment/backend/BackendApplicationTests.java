c717be0d5fcbf6a63ebde0f1a957fda1bf1839d8
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-04-30 11:41:00 -0500

    (backend) add default folder
    add default backend folder created with Spring Boot framework

---
+++ backend/src/test/java/com/CRM_Entertainment/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRM_Entertainment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
