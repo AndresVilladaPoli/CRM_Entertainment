b31817cc7042600e34ea2b90de82e00a312f89a3
MateoEcheverri <mateo_echeverri82201@elpoli.edu.co>
2024-05-05 13:05:25 -0500

    Login (Backend)
    Add Controler file for login

---
+++ backend/src/main/java/com/CRM_Entertainment/backend/Controllers/LoginController.java
@@ -0,0 +1,56 @@
+package com.CRM_Entertainment.backend.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginForm loginForm) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginForm.getEmail(), loginForm.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return ResponseEntity.ok("Logged in successfully!");
+    }
+
+    static class LoginForm {
+        private String email;
+        private String password;
+
+        // Getters and setters
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
+
